68d394053a6268aa96321b90165ae57240f8ba4f
Prox747 <carloproserpio1@gmail.com>
2024-06-07 23:45:19 +0200

    chore: correct indent

--- src/embedder.py
+++ src/embedder.py
@@ -65,8 +65,8 @@ def assign_device(device_name):
         if torch.cuda.is_available():
             print("CUDA is available. Using GPU.")
             print(f"CUDA Device Count: {torch.cuda.device_count()}")
-        for i in range(torch.cuda.device_count()):
-            print(f"Device {i}: {torch.cuda.get_device_name(i)}")
+            for i in range(torch.cuda.device_count()):
+                print(f"Device {i}: {torch.cuda.get_device_name(i)}")
         else:
             print("CUDA is not available. Falling back to CPU.")
             device = torch.device("cpu")
