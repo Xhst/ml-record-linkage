df0121aad2262d49597166eb877f257c491ae4e4
AlessandroPesare <ale.pesare@stud.uniroma3.it>
2024-06-14 20:21:48 +0200

    Refactoring preprocessing.py and preprocessing.ipynb

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -3,31 +3,35 @@ import json
 import torch
 import os
 import json
+import paths
 from difflib import SequenceMatcher 
+from collections import defaultdict
 
 
-DATASET_DIR = os.path.join(os.path.dirname(__file__), '../dataset/2013_monitor_specs')
+from collections import defaultdict
+from difflib import SequenceMatcher
 
-def get_dataset_dir():
-    return DATASET_DIR
-             
 def common_prefix_two_strings(str1, str2):
     matcher = SequenceMatcher(None, str1, str2)
     match = matcher.find_longest_match(0, len(str1), 0, len(str2))
     return str1[match.a: match.a + match.size]
 
-
-def find_longest_common_string(strings):
- 
-    if not strings:
+def find_longest_common_string(strings, percentage=0.2):
+    if not strings or percentage <= 0:
         return ""
-    prefix = strings[0]
-    for string in strings[1:]:
+
+    num_to_consider = int(len(strings) * percentage)
+    strings_to_compare = strings[:num_to_consider] 
+
+    if not strings_to_compare:
+        return ""
+
+    prefix = strings_to_compare[0]
+    for string in strings_to_compare[1:]:
         prefix = common_prefix_two_strings(prefix, string)
         if not prefix:
             break
     return prefix
-
 
 def process_directory(root_dir, source_dir):
     item2pagetitle = {}
@@ -50,7 +54,7 @@ def process_directory(root_dir, source_dir):
     longest_common_string = find_longest_common_string(list(item2pagetitle.values()))
     
     for key, value in item2pagetitle.items():
-        item2pagetitle[key] = value.replace(longest_common_string, '')
+            item2pagetitle[key] = value.replace(longest_common_string, '')
         
     return item2pagetitle
 
@@ -62,7 +66,7 @@ def process_sources(root_dir):
             item2pagetitle.update(process_directory(root_dir, dirname))
             
     try:
-        output_filepath = os.path.join(root_dir + '/../output/preprocessed.json')
+        output_filepath = os.path.join(paths.RESULTS_DIR + '/preprocessing/truncated_pagetitles.json')
         with open(output_filepath, 'w', encoding='utf-8') as f:
             json.dump(item2pagetitle, f, ensure_ascii=False, indent=4)
         print(f"Written to {output_filepath}")
