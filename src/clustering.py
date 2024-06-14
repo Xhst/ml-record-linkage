d121ba908ee7d4a6626aa0d28b778e95ce52baf9
CARLO PROSERPIO <115183769+Prox747@users.noreply.github.com>
2024-06-14 13:33:47 +0200

    refactor: hdbcan clusters now work with new embeddings
    Co-Authored-By: Mattia <82611659+Xhst@users.noreply.github.com>

--- src/clustering.py
+++ src/clustering.py
@@ -7,11 +7,11 @@ import torch
 from tqdm import tqdm
 
 
-def load_page_titles(file_path):
+def load_embeddings(file_dir, file_name):
     """
     Carica gli embedding da un file JSON.
     """
-    with open(file_path, 'r') as f:
+    with open(file_dir + "/" + file_name, 'r') as f:
         embeddings = json.load(f)
     return embeddings
 
@@ -26,14 +26,16 @@ def cluster_embeddings(embeddings, min_cluster_size=5, min_samples=1):
     return cluster_labels
 
 
-def save_clusters(embeddings, cluster_labels, output_dir, output_file):
+def save_clusters(item2embedding, cluster_labels, output_dir, output_file):
     """
     Salva i risultati del clustering in un file JSON.
     """
     print("Saving clusters to JSON...")
+    
     clusters = {}
-    for i, label in tqdm(enumerate(cluster_labels), total=len(cluster_labels), desc="Saving clusters"):
-        filename = list(embeddings.keys())[i]
+    for i, label in enumerate(cluster_labels):
+        filename = list(item2embedding.keys())[i]
+        
         if label not in clusters:
             clusters[label] = []
         clusters[label].append(filename)
@@ -44,64 +46,28 @@ def save_clusters(embeddings, cluster_labels, output_dir, output_file):
     with open(output_dir + "/" + output_file, 'w') as f:
         json.dump(clusters, f, indent=4)
     
-    print(f"Clusters saved successfully to {output_file}")
-
-
-def assign_device(device_name):
-    if device_name == "cuda":
-        if torch.cuda.is_available():
-            device = torch.device("cuda")
-            print("CUDA is available. Using GPU.")
-            print(f"CUDA Device Count: {torch.cuda.device_count()}")
-            for i in range(torch.cuda.device_count()):
-                print(f"Device {i}: {torch.cuda.get_device_name(i)}")
-        else:
-            print("CUDA is not available. Falling back to CPU.")
-            device = torch.device("cpu")
-    elif device_name == "mps":
-        if torch.backends.mps.is_available():
-            device = torch.device("mps")
-            print("MPS is available. Using Apple Silicon GPU.")
-        else:
-            print("MPS is not available. Falling back to CPU.")
-            device = torch.device("cpu")
-    else:
-        device = torch.device("cpu")
-    
-    # Display available devices
-    print(f"Device: {device}")
-    print(f"CUDA Available: {torch.cuda.is_available()}")
-    print(f"MPS Available: {torch.backends.mps.is_available()}")
-   
-    return device
+    print(f"Clusters saved successfully to " + output_dir + "/" + output_file)
 
 
 def main():
-    """
-    Punto di ingresso principale per il clustering degli embedding e il salvataggio dei risultati.
-    """
-    parser = argparse.ArgumentParser(description="Cluster embeddings and save the resulting clusters.")
-    parser.add_argument("--device", type=str, choices=["cpu", "cuda", "mps"], default="cpu", help="Computing device to use (cpu, cuda, or mps)")
-    args = parser.parse_args()
-    device = assign_device(args.device)
+    embeddings_dir = "results/embeddings/"
+    embeddings_file = "embeddings_distilbert_base_uncased.json"
     
-    embeddings_dir = "results/embeddings/embeddings.json"
-    item2pagetitle = load_page_titles(embeddings_dir)
+    item2embedding = load_embeddings(embeddings_dir, embeddings_file)
     print("Embeddings loaded successfully")
     
     start = time.time()
     
-    with tqdm(total=len(item2pagetitle), desc="Clustering") as pbar:
-        cluster_labels = cluster_embeddings(item2pagetitle)
-        pbar.update(len(item2pagetitle))
+    cluster_labels = cluster_embeddings(item2embedding)
 
     print("Clustering completed")
+    
     output_dir = "results/clustering/"
-    save_clusters(item2pagetitle, cluster_labels, output_dir, "hdbscan_clusters.json")
+    save_clusters(item2embedding, cluster_labels, output_dir, "hdbscan_clusters.json")
     print("Clusters saved successfully")
    
     end = time.time()
-    print(f"Processing time (using {device}): {end - start:.2f} seconds")
+    print(f"CLUSTERING --- Processing time: {end - start:.2f} seconds")
 
 
 if __name__ == "__main__":
