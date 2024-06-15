49d0924585f4935d37309ee1e0b1e83823e91112
Mattia <82611659+Xhst@users.noreply.github.com>
2024-06-15 07:47:49 +0000

    feat: string utils add whitespace between attached words

--- src/utils/string_utils.py
+++ src/utils/string_utils.py
@@ -23,3 +23,17 @@ def remove_extra_whitespaces(s: str) -> str:
         str: String with extra whitespaces removed.
     '''
     return ' '.join(s.split())
+
+
+def add_whitespace_between_attached_words(s: str) -> str:
+    '''
+    Add whitespace between attached words in a string.
+    It considers that words are attached when a lowercase letter is followed by an uppercase letter or a number.
+
+    Args:
+        s (str): Input string.
+
+    Returns:
+        str: String with whitespace added between attached words.
+    '''
+    return re.sub(r'([a-z])([A-Z0-9])', r'\1 \2', s)
