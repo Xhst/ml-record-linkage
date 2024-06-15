377cf3ca7b876d8fdf6461bef971cba5621aeb9a
Mattia <82611659+Xhst@users.noreply.github.com>
2024-06-15 15:38:49 +0000

    feat: processing word counter and clean text data

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -3,11 +3,58 @@ import json
 import torch
 import paths
 import utils.string_utils as string_utils
-from collections import defaultdict
+from collections import Counter
+
+
+def clean_text_data(text: str) -> str:
+    '''
+    Cleans the text data by converting it to lowercase, replacing special characters with whitespaces 
+    and removing extra whitespaces.
+    
+    Args:
+        text (str): The text data to be cleaned.
+    
+    Returns:
+        str: The cleaned text data.
+    '''
+    text = text.lower()
+    text = string_utils.replace_special_chars_with_whitespace(text, ['_', '.'])
+    text = string_utils.remove_extra_whitespaces(text)
+
+    return text
+
+
+def remove_common_words(source: str, item2pagetitle: dict[str, str], word_counter: Counter, min_percentage: float = 0.945):
+    '''
+    Removes common words from the page titles based on the minimum percentage of occurrences.
+
+    Args:
+        source (str): The source name.
+        item2pagetitle (Dict[str, str]): Dictionary containing item names and their corresponding page titles.
+        word_counter (Counter): Counter object containing the count of words in the page titles.
+        min_percentage (float): Minimum percentage of occurrences for a word to be considered common.
+    '''
+    min_occurrences = len(item2pagetitle) * min_percentage
+
+    words_to_remove = []
+    for word, count in word_counter.most_common():
+        if count < min_occurrences: continue
+        words_to_remove.append(word)
+    
+    print(f"Words to remove for {source}: {words_to_remove}")
+
+    for key, value in item2pagetitle.items():
+        for word in value.split():
+            if word not in words_to_remove: continue
+            value = value.replace(word, '')
+            value = string_utils.remove_extra_whitespaces(value)
+            item2pagetitle[key] = value
 
 
 def process_directory(root_dir, source_dir):
     item2pagetitle = {}
+    word_counter = Counter()
+
     for _, _, files in os.walk(root_dir + '/' + source_dir):
         for file in files:
             filepath = os.path.join(root_dir + '/' + source_dir, file)
@@ -17,24 +64,17 @@ def process_directory(root_dir, source_dir):
                     if data:
                         item_name = source_dir + '//' + file.replace('.json', '')
                         page_title = data['<page title>']
-                        
-                        item2pagetitle[item_name] = page_title
+
+                        cleaned_page_title = clean_text_data(page_title)
+                        word_counter.update(cleaned_page_title.split())
+
+                        item2pagetitle[item_name] = cleaned_page_title
                     else:
                         print(f"Empty JSON file: {filepath}")
             except Exception as e:
                 print(f"Error reading {filepath}: {e}")
-                
-    prefix = string_utils.find_longest_common_prefix(item2pagetitle.values())
-    suffix = string_utils.find_longest_common_suffix(item2pagetitle.values())
 
-    for key, value in item2pagetitle.items():
-        value = value.replace(prefix, '')
-        value = value.replace(suffix, '')
-        value = string_utils.replace_special_chars_with_whitespace(value, ['_', '.'])
-        value = string_utils.remove_extra_whitespaces(value)
-        value = value.lower()
-
-        item2pagetitle[key] = value
+    remove_common_words(source_dir, item2pagetitle, word_counter)      
         
     return item2pagetitle
 
@@ -44,7 +84,7 @@ def process_sources(root_dir):
     for _, dirnames, _ in os.walk(root_dir):
         for dirname in dirnames:
             item2pagetitle.update(process_directory(root_dir, dirname))
-            
+    
     try:
         output_filepath = os.path.join(paths.RESULTS_DIR + '/preprocessing/preprocessed_pagetitle.json')
         with open(output_filepath, 'w', encoding='utf-8') as f:
