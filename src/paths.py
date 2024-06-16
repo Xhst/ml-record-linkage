60d66af86edebeafd4fc72ce3f3de4e6fba01f45
CARLO PROSERPIO <115183769+Prox747@users.noreply.github.com>
2024-06-16 19:18:27 +0200

    feat: evaluation script
    Co-Authored-By: Mattia <82611659+Xhst@users.noreply.github.com>

--- src/paths.py
+++ src/paths.py
@@ -1,4 +1,5 @@
 import os
 
 DATASET_DIR = os.path.join(os.path.dirname(__file__), '../dataset/2013_monitor_specs')
+GROUND_TRUTH_DIR = os.path.join(os.path.dirname(__file__), '../dataset/ground_truth')
 RESULTS_DIR = os.path.join(os.path.dirname(__file__), '../results')
