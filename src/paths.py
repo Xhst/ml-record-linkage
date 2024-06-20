4309995908be5c9c1d878b7a9398b0f0c84ae70e
CARLO PROSERPIO <115183769+Prox747@users.noreply.github.com>
2024-06-20 16:16:57 +0200

    added model dir

--- src/paths.py
+++ src/paths.py
@@ -3,3 +3,4 @@ import os
 DATASET_DIR = os.path.join(os.path.dirname(__file__), '../dataset/2013_monitor_specs')
 GROUND_TRUTH_DIR = os.path.join(os.path.dirname(__file__), '../dataset/ground_truth')
 RESULTS_DIR = os.path.join(os.path.dirname(__file__), '../results')
+MODELS_DIR = os.path.join(os.path.dirname(__file__), '../models')
