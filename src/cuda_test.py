ee43617f241ac5ee7b71d1686705630c5ea3c48f
Prox747 <carloproserpio1@gmail.com>
2024-06-27 12:59:44 +0200

    changed gpu layers

--- src/cuda_test.py
+++ src/cuda_test.py
@@ -1,6 +1,6 @@
 from llama_cpp import Llama
 import paths
-llm = Llama(model_path=paths.MODELS_DIR + "/Llama3-70B/L3-70B-Euryale-v2.1-Q5_K_M.gguf", n_gpu_layers=30, n_ctx=3584, n_batch=521, verbose=True)
+llm = Llama(model_path=paths.MODELS_DIR + "/Llama3-70B/L3-70B-Euryale-v2.1-IQ3_XXS.gguf", n_gpu_layers=30, n_batch=521, n_ctx=2048, verbose=True)
 # adjust n_gpu_layers as per your GPU and model
 output = llm("Q: Name the planets in the solar system? A: ", max_tokens=32, stop=["Q:", "\n"], echo=True)
 print(output)
