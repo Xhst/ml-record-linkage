edd5f7425c78e2684c519df3eb5a67054a74d59f
Mattia <82611659+Xhst@users.noreply.github.com>
2024-08-11 10:32:51 +0200

    feat: siamese network

---
+++ src/pairwise_matching_siamese.py
@@ -0,0 +1,40 @@
+import json
+import paths
+import torch
+import torch.nn as nn
+from torch.utils.data import DataLoader, Dataset
+
+
+class SiameseNetwork(nn.Module):
+    def __init__(self, embedding_dim, hidden_dim):
+        super(SiameseNetwork, self).__init__()
+        
+        self.embedding_dim = embedding_dim
+        self.hidden_dim = hidden_dim
+        
+        self.input_layer = nn.Linear(embedding_dim, hidden_dim)
+        self.hidden_layer = nn.Linear(hidden_dim, hidden_dim)
+        self.output_layer = nn.Linear(hidden_dim, 1)
+        
+        self.relu = nn.ReLU()
+        self.sigmoid = nn.Sigmoid()
+        
+
+    def forward_once(self, x):
+        x = self.input_layer(x)
+        x = self.relu(x)
+        x = self.hidden_layer(x)
+        x = self.relu(x)
+        x = self.output_layer(x)
+        x = self.sigmoid(x)
+
+        return x
+    
+
+    def forward(self, input1, input2):
+        output1 = self.forward_once(input1)
+        output2 = self.forward_once(input2)
+
+        return output1, output2
+    
+
