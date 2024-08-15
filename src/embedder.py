a068711d0d4dd9e9d888343b91142f8f5f7f3242
Prox747 <carloproserpio1@gmail.com>
2024-08-15 21:58:57 +0200

    fix: output file name was not changed

--- src/embedder.py
+++ src/embedder.py
@@ -123,7 +123,6 @@ def main():
     print("Saving to json...")
     
     output_dir = "results/embeddings/"
-    output_file_name = "embeddings_distilbert_base_uncased.json"
     
     save_embedding_dictionary(embeddings, output_dir, output_file_name)
     
